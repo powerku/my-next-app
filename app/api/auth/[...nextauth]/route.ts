81d3d209d811d0704bbaf1eea0b604e0326e16ef
griffin <griffin.ku@kakaocorp.com>
2023-10-26 14:44:42 +0900

    add: next-auth 소셜 로그인 구현하기

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import KakaoProvider from "next-auth/providers/kakao";
+export const authOptions = {
+    // Configure one or more authentication providers
+    providers: [
+        KakaoProvider({
+            clientId: process.env.KAKAO_CLIENT_ID ?? "",
+            clientSecret: process.env.KAKAO_CLIENT_SECRET ?? "",
+        }),
+        // ...add more providers here
+    ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import QueryProvider from '@/components/providers/query-provider'
+import AuthSession from '@/components/providers/session-provider'
+import LoginButton from '@/components/login-button'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +20,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <AuthSession>
       <QueryProvider>
+          <div className="h-[48px] bg-black flex items-center">
+              <h1 className="text-white">powerku</h1>
+              <ul className="ml-auto mr-5">
+                   <li><LoginButton></LoginButton></li>
+              </ul>
+          </div>
         {children}
       </QueryProvider>
+      </AuthSession>
+
       </body>
     </html>
   )
